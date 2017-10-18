5a0a2535eebd932b99c189006dcdf5cff83b2fc9
Alex Kleider <alex@x301.4>
2017-10-17 23:30:25 -0700

    First FT specced out in comments and uses unittest

--- ft.py
+++ ft.py
@@ -1,8 +1,45 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewUserTest(unittest.TestCase):
+    """
+    Any method with a name beginning with 'test' is
+    a test method and will be run by the test runner.
+    """
 
-assert "Django" in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        """
+        tearDown will always run unless there's an error in setUp
+        """
+        self.browser.close()
 
-browser.close()
+# Our user has heard about the double entry book keeping site
+# and elects to try it out by going to the site:
+
+    def test_can_enter_entities_and_see_them_listed(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("Double Entry Book Keeping", self.browser.title)
+        self.fail("Finish the test!")
+
+# The home page provides her with the option of creating
+# an 'entity'.
+
+# She creates 'FirstEntity' and sees that it appears on the page
+# in a 'List of Entities'.
+
+# There is still a text box allowing for creation of another.
+# She creates 'SecondEntity' and sees it added to the list.
+
+# Will the site remember her list??
+# The site has generated a unique URL for her.
+
+# Visiting that URL reveals her list.
+
+# Satisfied she goes back to sleep.
+
+if __name__ == '__main__':
+#   unittest.main(warnings='ignore')  # launches the test runner
+    unittest.main()  # launches the test runner
