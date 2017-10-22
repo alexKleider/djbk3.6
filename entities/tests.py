1860268d2639117d508cc9251f680bbe2a04e8fa
Alex <akleider@sonic.net>
2017-10-22 15:40:21 -0700

    Refactor to use template.
    Had to register app so Django would see the template. (end p46)

--- entities/tests.py
+++ entities/tests.py
@@ -24,4 +24,4 @@ class HomePageTest(TestCase):
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>Double Entry Book Keeping</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
